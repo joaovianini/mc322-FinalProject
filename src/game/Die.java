03f1c9ee1be6a97e8dd13aaed723bba44ac4cfb5
João Vianini <joaovianini@me.com>
2020-07-21 21:53:03 -0300

    Inclusão do Dado

---
+++ src/game/Die.java
@@ -0,0 +1,40 @@
+package game;
+
+import java.security.SecureRandom;
+
+public class Die {
+	private DieFaces dieFaces;
+	private final static SecureRandom random = new SecureRandom();
+	
+	public Die() {
+
+	}
+	
+	public DieFaces rollDice() {
+		int face;
+		face = 1 +  random.nextInt(6);
+		switch(face) {
+		case 1:
+			dieFaces = DieFaces.SKULL;
+			break;
+		case 2:
+			dieFaces = DieFaces.SKULL;
+			break;
+		case 3:
+			dieFaces = DieFaces.SKULL;
+			break;
+		case 4:
+			dieFaces = DieFaces.HEROSHIELD;
+			break;
+		case 5:
+			dieFaces = DieFaces.HEROSHIELD;
+			break;
+		case 6: 
+			dieFaces = DieFaces.MONSTERSHIELD;
+			break;
+		}
+		return dieFaces;
+	}
+	
+}
+
---
+++ src/game/DieFaces.java
@@ -0,0 +1,5 @@
+package game;
+
+public enum DieFaces {
+	SKULL, HEROSHIELD, MONSTERSHIELD;
+}
